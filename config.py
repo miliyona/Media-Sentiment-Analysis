e5aa4ff36bd7ce68fd8aeaf966ceeb71a9c75ce0
miliyona <miliyona@outlook.com>
2018-06-30 12:41:18 -0600

    adding my homework files

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "bzyymMHOEsq6OfWTPNeK25dht"
+consumer_secret = "re4OzcRbve7Jto0GpIgfTHZyCTDRHcc809kfVXt2bzVlaZxB7e"
+access_token = "1009180271843741696-J9YbDgvasiyNCxNNZOKVxrVluTyyTa"
+access_token_secret = "fpRO54VUFlAyzjU7lvU4xPbMNlAVYKdGQbF1p3oNx4lf2"
